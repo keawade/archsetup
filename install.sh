d9555d032022599a0e8423d19afadc47193f6e92
Keith Wade <keawade@users.noreply.github.com>
2015-06-24 10:14:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,20 @@
 # Git: https://www.github.com/keawade
 # Version: 0.0.1
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 echo "Welcome to Arch!"
 echo "This script will guide you through your Arch installation."
 read -p "Please press [Enter] to begin..."
 
+# Check for internet connectivity
+echo "Checking for network connectivity..."
 wget -q --tries=4 --timeout=4 --spider http://google.com
 if [[ $? -eq 0 ]]; then
-  echo "Network connection online"
+  echo "[${GREEN}SUCCESS${NC}] Network connection online"
 else
-  echo "Please check your network connection"
+  echo "[${RED}FAILED${NC}] Please check your network connection"
   exit 0
 fi
